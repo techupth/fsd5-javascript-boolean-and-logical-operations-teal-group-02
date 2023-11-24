9b57bc46736e7963ddb3243418c272b33dd6f1c6
Dennis <suwatchai.kaew@gmail.com>
2023-11-24 13:52:28 +0700

    fix: edit variable declaration

--- ex-2.js
+++ ex-2.js
@@ -1,13 +1,21 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000 = true;
-let isWeekday = true;
-let hasBoughtProductFromITCategory = false;
-let hasAttendedDiscountEvent = true;
-let isPlatinum = false;
+let lastMonthPaidMoreThan4000;
+let isWeekday;
+let hasBoughtProductFromITCategory;
+let hasAttendedDiscountEvent;
+let isPlatinum;
 
-let hasPromotion =
+let hasPromotion;
+
+lastMonthPaidMoreThan4000 = true;
+isWeekday = true;
+hasBoughtProductFromITCategory = false;
+hasAttendedDiscountEvent = true;
+isPlatinum = false;
+
+hasPromotion =
   (lastMonthPaidMoreThan4000 &&
     isWeekday &&
     !hasBoughtProductFromITCategory &&
